0699d7ef4896bc5842c36420752e8b45d72ae779
starminalush <alica154323@gmail.com>
2023-04-27 02:04:31 +0500

    Add minio in docker. Add script for downloading fer2013 dataset. Add ci workflow.

---
+++ experiments/fer/data/download_dataset.py
@@ -0,0 +1,24 @@
+import click
+from dotenv import load_dotenv
+from loguru import logger
+
+load_dotenv()
+from kaggle import api  # noqa: E402
+
+api.authenticate()
+
+
+@click.command()
+@click.argument("external_dataset_path", type=click.Path())
+def download_dataset(external_dataset_path: str):
+    """Download external dataset from kaggle. Link: https://www.kaggle.com/datasets/msambare/fer2013.
+    Args:
+        external_dataset_path: download dataset path
+    """
+    ds_name: str = "msambare/fer2013"
+    api.dataset_download_files(ds_name, path=external_dataset_path, unzip=True)
+    logger.info("finish download dataset")
+
+
+if __name__ == "__main__":
+    download_dataset()
