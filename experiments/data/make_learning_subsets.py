d3b544a0e9cb5090173a49d68167874824781676
starminalush <alica154323@gmail.com>
2023-05-25 01:15:00 +0500

    add simple eda affectnet notebook

--- experiments/data/make_learning_subsets.py
+++ experiments/data/make_learning_subsets.py
@@ -9,10 +9,16 @@ import splitfolders
 @click.argument("output-dataset-dir", type=click.Path())
 def make_learning_subsets(dataset_dir: Path | str, output_dataset_dir: Path | str):
     dataset_dir = Path(dataset_dir)
+    output_dataset_dir = Path(output_dataset_dir)
     Path(output_dataset_dir).mkdir(exist_ok=True, parents=True)
     splitfolders.ratio(
         dataset_dir, output=output_dataset_dir, seed=42, ratio=(0.8, 0.1, 0.1)
     )
+    for subset in ["train", "val", "test"]:
+        (output_dataset_dir / subset / "angry").parent.mkdir(exist_ok=True)
+        (output_dataset_dir / subset / "anger").rename(
+            output_dataset_dir / subset / "angry"
+        )
 
 
 if __name__ == "__main__":
